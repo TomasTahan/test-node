5bf400fe923c00bf2530814d42576c121a3edc3d
TomasTahan <133704578+TomasTahan@users.noreply.github.com>
2025-08-03 17:19:15 -0400

    link

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const axios = require("axios");
     console.log("Polling en:", new Date().toLocaleTimeString());
     
     // Simulando una llamada asíncrona
-    const resultado = await axios.get("https://api.ejemplo.com/dato");
+const resultado = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
     console.log("Resultado:", resultado.data);
   } catch (error) {
     console.error("Error en el polling:", error);
